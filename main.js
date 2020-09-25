ecdf93b374b9faba4b686d6dc5158bc54956d387
tala agha <you@example.c>
2020-09-25 23:30:29 +0300

    adding new project

---
+++ main.js
@@ -0,0 +1,9 @@
+/**************Changing the Image ***********************/
+function imgSlider(anything) {
+  document.querySelector(".pepsi").src = anything;
+}
+/*************Changing the Backgorundcolor **************/
+function changeBgColor(color) {
+  const sec = document.querySelector(".sec");
+  sec.style.background = color;
+}
